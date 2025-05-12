d742592aec665169ac2fa17c708b274eb260621a
Jennifer Morales Barbero <118901667+JMoralBa@users.noreply.github.com>
2025-05-12 13:36:13 +0200

    initial commit

---
+++ R/NSDM.inlabru.R
@@ -0,0 +1,11 @@
+#' NSDM.inlabru
+#'
+#' Estima probabilidad o intensidad mediante modelos jerárquicos bayesianos con INLA e inlabru.
+#'
+#' @param nsdm_obj Objeto de clase `nsdm.vinput`.
+#' @param output "probability" o "intensity".
+#' @return Lista con los resultados del modelo ajustado.
+#' @export
+NSDM.inlabru <- function(nsdm_obj, output = "probability") {
+  stop("Función aún no implementada")
+}
